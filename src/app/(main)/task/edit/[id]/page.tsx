fb41db6765f9c2c28f10175acdd7d3e1b4799679
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-05 23:16:23 +0900

    fix: route ids

--- src/app/(main)/task/edit/[id]/page.tsx
+++ src/app/(main)/task/edit/[id]/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+export function generateStaticParams() {
+  return [{ id: [1,2,3,4,5] }];
+}
+
 const TaskEditIDPage = ({ params }: {
     params: { id: string }
 }) => {
