9c00af4e828ce690a2d3b299fd72fcc6847177da
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-11 19:13:27 +0900

    feat: form actions

--- src/actions/createActions.tsx
+++ src/actions/createActions.tsx
@@ -1,12 +1,14 @@
 'use server';
 
-export const sampleAction = async (name: string) => {
-    console.log('サーバーアクションが実行されました:', name);
-};
+interface FormState {
+    error: string;
+}
 
-export const createTask = async (taskId: number, formData: FormData) => {
+export const createTask = async (taskId: number, state:FormState, formData: FormData) => {
     // DBにタスクを作成する
     console.log('タスクが作成されました');
     console.log('Task ID:', taskId);
     console.log(formData.get('name'));
+    state.error = 'サーバーエラーが発生しました。';
+    return state;
 }
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,14 +1,30 @@
+'use client';
+
+import { useFormState, useFormStatus } from 'react-dom';
+
 import { createTask } from "@/actions/createActions"
 
 const ServerActionsPage = () => {
-    const id = 1;
-    const boundCreateTaskWithId = createTask.bind(null, id);
+  const id = 1;
+  const createTaskWithId = createTask.bind(null, id);
+  const initialState = { error: '' }
+  const [state, formAction] = useFormState(createTaskWithId, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <button type="submit" className="bg-gray-400 disabled:bg-gray-300 ml-2 px-2" disabled={pending}>
+        {pending ? '送信中' : '送信'}
+      </button>
+    )
+  }
   return (
     <div>
-        <form action={boundCreateTaskWithId}>
-            <input type="text" name="name" id="name" className="border bg-gray-300" /> 
-            <button type="submit" className="bg-gray-400 ml-2 px-2">送信</button>
-        </form>
+      <form action={formAction}>
+        <input type="text" name="name" id="name" className="border bg-gray-300" />
+        <SubmitButton />
+        <div>{state.error}</div>
+      </form>
     </div>
   )
 }
