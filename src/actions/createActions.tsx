b9f2439611ae0399f5b5c43873e337c467ee444d
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-11 18:23:14 +0900

    feat: server actions

--- src/actions/createActions.tsx
+++ src/actions/createActions.tsx
@@ -4,8 +4,9 @@ export const sampleAction = async (name: string) => {
     console.log('サーバーアクションが実行されました:', name);
 };
 
-export const createTask = async (formData: FormData) => {
+export const createTask = async (taskId: number, formData: FormData) => {
     // DBにタスクを作成する
     console.log('タスクが作成されました');
+    console.log('Task ID:', taskId);
     console.log(formData.get('name'));
 }
--- src/app/sa/page.tsx
+++ src/app/sa/page.tsx
@@ -1,9 +1,11 @@
 import { createTask } from "@/actions/createActions"
 
 const ServerActionsPage = () => {
+    const id = 1;
+    const boundCreateTaskWithId = createTask.bind(null, id);
   return (
     <div>
-        <form action={createTask}>
+        <form action={boundCreateTaskWithId}>
             <input type="text" name="name" id="name" className="border bg-gray-300" /> 
             <button type="submit" className="bg-gray-400 ml-2 px-2">送信</button>
         </form>
