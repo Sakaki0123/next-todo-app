f77ccff755e234866cdb7b0a381fbaac499e7054
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-12 18:04:13 +0900

    feat: add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+import Link from "next/link";
+
+const ErrorPage = () => {
+    return (
+        <div className="h-screen flex flex-col justify-center items-center bg-slate-50 text-gray-900">
+            <h1 className="text-8xl font-bold p-8">Error</h1>
+            <p className="text-4xl font-medium">Unexpected Error Occurred</p>
+            <Link href="/" className="mt-4 text-xl text-blue-600 hover:underline">Go back home</Link>
+        </div>
+    )
+}
+
+export default ErrorPage
