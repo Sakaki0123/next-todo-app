c9037155c7ced808fa10dbbb16381f17b231e205
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-12 18:11:02 +0900

    feat: add mongodb

---
+++ src/utils/database.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI as string || "");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw new Error();
+    }
+
+}
