843c9aef7b534f320b31cf923d3edc2d91eddc32
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-12 18:33:28 +0900

    feat: create page

--- src/components/NewTaskForm/NewTaskForm.tsx
+++ src/components/NewTaskForm/NewTaskForm.tsx
@@ -1,9 +1,23 @@
+'use client';
 
+import { createTask, FormState } from "@/actions/task";
+import { useFormState, useFormStatus } from "react-dom";
 
 const NewTaskForm = () => {
+    const initialState: FormState = { error: "" };
+    const [state, formAction] = useFormState(createTask, initialState);
+
+    const SubmitButton = () => {
+        const { pending } = useFormStatus();
+        return (
+            <button type="submit" className="mt-8 py-2 w-full bg-gray-800 text-white rounded-md hover:bg-gray-700 font-semibold disabled:bg-gray-400" disabled={pending}>
+                Create
+            </button>
+        )
+    }
     return (
         <div className="mt-10 mx-auto w-full max-w-sm">
-            <form action="">
+            <form action={formAction}>
                 <div>
                     <label htmlFor="title" className="block text-sm font-medium">
                         タイトル
@@ -23,9 +37,8 @@ const NewTaskForm = () => {
                     <input type="date" name="dueDate" id="dueDate" min=
                         "2020-01-01" max="2999-12-31" required className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300" />
                 </div>
-                <button type="submit" className="mt-8 py-2 w-full bg-gray-800 text-white rounded-md hover:bg-gray-700 font-semibold">
-                    Create
-                </button>
+                <SubmitButton />
+                {state.error && (<p className="mt-2 text-red-500 text-sm">{state.error}</p>)}
             </form>
         </div>
     )
