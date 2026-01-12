49b28d2a8c3f71d2959b9d05568b591d8db7c3dd
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-12 17:49:55 +0900

    feat: edit page

--- src/components/EditTaskForm/EditTaskForm.tsx
+++ src/components/EditTaskForm/EditTaskForm.tsx
@@ -1,6 +1,6 @@
 
 
-const NewTaskForm = () => {
+const EditTaskForm = () => {
     return (
         <div className="mt-10 mx-auto w-full max-w-sm">
             <form action="">
@@ -23,12 +23,16 @@ const NewTaskForm = () => {
                     <input type="date" name="dueDate" id="dueDate" min=
                         "2020-01-01" max="2999-12-31" required className="block mt-2 py-1.5 px-2 w-full rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300" />
                 </div>
+                <div className="mt-6 flex items-center">
+                    <input type="checkbox" name="isCompleted" id="isCompleted" className="mr-2 w-4 h-4" />
+                    <label htmlFor="isCompleted" className="ml-2 text-sm">タスクを完了にする</label>
+                </div>
                 <button type="submit" className="mt-8 py-2 w-full bg-gray-800 text-white rounded-md hover:bg-gray-700 font-semibold">
-                    Create
+                    Edit
                 </button>
             </form>
         </div>
     )
 }
 
-export default NewTaskForm
+export default EditTaskForm
