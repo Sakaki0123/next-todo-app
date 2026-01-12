398fc63f67b1a57516e43aaa5bca3137c958c644
Ky Skk <kyskk@Kynonotobukkukonpyuta.local>
2026-01-12 18:49:36 +0900

    feat: all tasks view page

--- src/components/SideMenu/TaskCard/TaskCard.tsx
+++ src/components/SideMenu/TaskCard/TaskCard.tsx
@@ -1,18 +1,22 @@
+import { TaskDocument } from "@/models/task";
 import TaskDeleteButton from "./TaskDeleteButton/TaskDeleteButton"
 import TaskEditButton from "./TaskEditButton/TaskEditButton"
 
-const TaskCard = () => {
+interface TaskCardProps {
+    task: TaskDocument;
+}
+const TaskCard: React.FC<TaskCardProps> = ({ task }) => {
     return (
         <div className="w-64 h-52 p-4 bg-white rounded-md shadow-md flex flex-col justify-between">
             <header>
-                <h1 className="text-lg font-semibold">Title</h1>
-                <div className="mt1 text-sm line-clamp-3">タスクの説明</div>
+                <h1 className="text-lg font-semibold">{task.title}</h1>
+                <div className="mt1 text-sm line-clamp-3">{task.description}</div>
             </header>
             <div>
-                <div className="text-sm">2024-12-31</div>
+                <div className="text-sm">{task.dueDate}</div>
                 <div className="flex justify-between items-center">
-                    <div className={`mt-1 text-sm px-2 py-1 w-24 text-center text-white rounded-full shadow-sm ${true ? 'bg-green-500' : 'bg-red-500'}`}>
-                        {true ? 'completed' : 'imcomplete'}
+                    <div className={`mt-1 text-sm px-2 py-1 w-24 text-center text-white rounded-full shadow-sm ${task.isCompleted ? 'bg-green-500' : 'bg-red-500'}`}>
+                        {task.isCompleted ? 'completed' : 'imcomplete'}
                     </div>
                     <div className="flex gap-4">
                         <TaskEditButton id='1'/>
